ce9de5a5f406e58228ad98d21b1d1adb1d408b31
HJM-Depaul <bjhj0230@gmail.com>
2024-12-05 01:55:26 -0500

    indentation/Fixed+add Nav dropdown/Fixed CSS

--- script.js
+++ script.js
@@ -11,6 +11,43 @@ const preview_buttonRight = document.querySelector(
 const preview_title = document.querySelector("#preview_title_id");
 const preview_description = document.querySelector("#preveiw_description_id");
 
+const projects_first_iframe = document.querySelector(".firstProjects_iframe");
+
+let allProjects_iframe_scrs = [
+  //0
+  "https://drive.google.com/file/d/1GJpSN39ii04dp4zrayqRTauoVVLLIXfI/preview",
+  //1
+  "https://drive.google.com/file/d/1NdTlpD9g0AbyFRbixx88IPD2MjzY5XS9/preview",
+  //2
+  "https://drive.google.com/file/d/19z4l7FkBPJqWShlrRpGXN254tatqc8MI/preview",
+  //3
+  "https://drive.google.com/file/d/1wQQ3j1Bk8AzQxfskuSxzzi7y6dGcAS1j/preview",
+  //4
+  "https://drive.google.com/file/d/1HYpFrunGodYARe61KdJJI9RqS4uE9pfg/preview",
+  //5
+  "https://drive.google.com/file/d/12aCz6WTcLpCAeXN7vHf11UrflECufaQ2/preview"
+];
+
+
+//header selector
+const headerWrap = document.querySelector("#header_id");
+//nav bar selector
+const nav = document.querySelector(".navigation_bar");
+
+// hide header banner
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 50) {
+    headerWrap.classList.add("invisible");
+    nav.classList.add("fixedTop");
+
+  } else {
+    headerWrap.classList.remove("invisible");
+    nav.classList.remove("fixedTop");
+
+  }
+});
+
+
 //logo hover animation
 //https://stackoverflow.com/questions/75606570/how-to-make-a-video-play-only-on-hover-using-html-css
 logo_button.addEventListener("mouseover", function () {
@@ -19,7 +56,7 @@ logo_button.addEventListener("mouseover", function () {
 });
 logo_button.addEventListener("mouseleave", function () {
   this.pause();
-  logo_button.currentTime = 2;
+  logo_button.currentTime = 4;
 });
 
 // Project Class
@@ -54,15 +91,29 @@ let allProjectsPreview = [
   ),
   new Project(
     "Fourth Temporary iframe",
-    "https://www.youtube.com/embed/Ej_nZ6E-rDQ?autoplay=1&mute=1",
+    "https://drive.google.com/file/d/12aCz6WTcLpCAeXN7vHf11UrflECufaQ2/preview",
     "Temporary Embeded YouTube Video - Practice URL parameters",
     "I hope I can code well!"
   ),
 ];
 
-//load all img first prevent delays
+
+for (let i = allProjects_iframe_scrs.length - 1; i >= 0; i--) {
+  const iframe = document.createElement('iframe');
+  iframe.src = allProjects_iframe_scrs[i];
+  iframe.onload = () => {
+    // Append the iframe to the desired container
+    document.body.appendChild(iframe);
+  };
+}
+
+// load all img first prevent delays
 allProjectsPreview.forEach((currentProject) => {
-  preview_img.src = currentProject.imgSrc;
+  const img = new Image();
+  img.src = currentProject.imgSrc;
+  img.onload = () => {
+    preview_img.src = currentProject.imgSrc;
+  };
 });
 
 let currentLength = allProjectsPreview.length;
